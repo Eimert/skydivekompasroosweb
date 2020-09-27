7e2b0506a3e137a39605748e3d60364ad92c82e6
Eimert <Eimert@users.noreply.github.com>
2020-09-27 16:24:00 +0000

    deploy: ed924357c4b307e351f23529f1ecac54ac81776e

---
+++ precache-manifest.eac744bf8ad1565231a7836c4d91bfc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d580db5ac4ea545905cf3f415108584e",
+    "url": "/skydivekompasroosweb/index.html"
+  },
+  {
+    "revision": "08527610f7983cb3bea0",
+    "url": "/skydivekompasroosweb/static/css/main.75452755.chunk.css"
+  },
+  {
+    "revision": "0e9b8c19787f63ef02e1",
+    "url": "/skydivekompasroosweb/static/js/2.13a1160f.chunk.js"
+  },
+  {
+    "revision": "4e3f8d4bad1db285aba0fceffdc7b967",
+    "url": "/skydivekompasroosweb/static/js/2.13a1160f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08527610f7983cb3bea0",
+    "url": "/skydivekompasroosweb/static/js/main.e52b8c19.chunk.js"
+  },
+  {
+    "revision": "24385764ff45e6951f58",
+    "url": "/skydivekompasroosweb/static/js/runtime-main.cfbd9755.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.307f8046ae4c88768a99242925f52267.js"
+  "/skydivekompasroosweb/precache-manifest.eac744bf8ad1565231a7836c4d91bfc8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/skydivekompasroosweb/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
