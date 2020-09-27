d87883cbf4a358abe7a56ca2730486f3b0715fae
Eimert Vink <eimertvink@gmail.com>
2020-09-27 18:05:59 +0200

    Updates

--- precache-manifest.307f8046ae4c88768a99242925f52267.js
+++ precache-manifest.307f8046ae4c88768a99242925f52267.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2e08dff11eade26186db0db0e143f225",
+    "revision": "0a6d96641f58538fc769a56e0556af10",
     "url": "/index.html"
   },
   {
-    "revision": "3dda1758028817e7c96c",
+    "revision": "18dd3931871f25c84ae0",
     "url": "/static/css/main.75452755.chunk.css"
   },
   {
-    "revision": "0e9b8c19787f63ef02e1",
-    "url": "/static/js/2.13a1160f.chunk.js"
+    "revision": "9aa321e478fc57351975",
+    "url": "/static/js/2.b3634f14.chunk.js"
   },
   {
     "revision": "4e3f8d4bad1db285aba0fceffdc7b967",
-    "url": "/static/js/2.13a1160f.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.b3634f14.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3dda1758028817e7c96c",
-    "url": "/static/js/main.d2320bcd.chunk.js"
+    "revision": "18dd3931871f25c84ae0",
+    "url": "/static/js/main.8e0a3844.chunk.js"
   },
   {
     "revision": "d477b4dbf8a89cef76c5",
